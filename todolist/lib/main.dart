e45ef8449648f09b02979231e1a3f26d59d11f52
Daekeun Ko <kdkcom1234@gmail.com>
2023-08-18 13:21:06 +0900

    first commit

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -14,13 +14,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomeWidget(),
     );
   }
 }
 
 class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
+
   @override
   State<StatefulWidget> createState() {
     return HomeWidgetState();
